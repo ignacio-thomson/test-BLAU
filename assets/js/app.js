676822b621a9d1bc20d586bdb39f53befa70c357
Ignacio <ignacio.thomson.97@gmail.com>
2023-02-23 12:46:07 -0300

    Code has been refactorized.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+// Se importa el Array de objetos desde ./business.js
+import { businesses } from "./business.js";
+
 // Método básico para cambiar el color del icono LIKE
 function toggleHeartColor(heartButton) {
   const heartIcon = heartButton.querySelector("i");
@@ -11,3 +14,57 @@ function toggleHeartColor(heartButton) {
     likeCount.textContent = parseInt(likeCount.textContent) - 1;
   }
 }
+
+// Se selecciona el contenedor donde se guardarán las Cards
+const container = document.querySelector(".my-container");
+
+// Se itera sobre el array de objetos 'businesses', por cada objeto se genera dinámicamente una Card
+businesses.forEach((item) => {
+  const newCard = `
+    <div class="card border border-secondary">
+      <div class="ps-3 pt-3">
+        <h5 class="card-title">
+          <i
+            class="${item.icon} text-danger border border-2 border-secondary rounded-circle p-2"
+          ></i>
+          &nbsp;${item.name}
+        </h5>
+      </div>
+      <button
+        onclick="bootbox.alert('${item.description}. <br/><b>Teléfono:</b> ${item.phone}. <br/><b>Correo:</b> ${item.email}')"
+      >
+        <img
+          src="${item.img}"
+          class="card-img-top rounded-8 p-3"
+          alt="Tutor and student"
+        />
+      </button>
+      <div class="card-body">
+        <h5 class="card-title">
+          <button class="heart-button">
+            <i class="fa-solid fa-heart heart-icon"></i>
+          </button>
+          <span class="like-count">0</span>
+          - ${item.category}
+        </h5>
+        <br />
+        <p class="card-text border border-secondary rounded-6 p-1">
+        &nbsp;&nbsp;<b>Etiquetas:</b> ${item.tags}
+        </p>
+      </div>
+    </div>
+`;
+  const card = document.createElement("li");
+  card.innerHTML = newCard;
+  // Se inserta la nueva Card al contenedor 'my-container'
+  container.appendChild(card);
+});
+
+// Se selecciona el botón de 'Like'
+const heartIcon = document.querySelectorAll(".heart-button");
+// Se añade un eventListener al botón de 'Like' que llama la función para cambiar de color el icono de corazón
+heartIcon.forEach((e) => {
+  e.addEventListener("click", function () {
+    toggleHeartColor(e);
+  });
+});
---
+++ assets/js/business.js
@@ -0,0 +1,101 @@
+// Array de Objetos con la información
+export const businesses = [
+  {
+    name: "Duck Spa",
+    category: "Salón de Belleza",
+    tags: "belleza, centro, cabello",
+    img: "assets/img/card-img/blake-wisz-GFrBMipOd_E-unsplash.jpg",
+    icon: "fa-solid fa-scissors",
+    description:
+      "Peluquería Duck Spa, encargada del corte de pelo de las celebridades más reconocidas del país. Agenda abierta.",
+    phone: "111-1111",
+    email: "duckspa@gmail.com",
+  },
+  {
+    name: "Duck Pizzeria",
+    category: "Pizzería",
+    tags: "pizza, gourmet, restaurant",
+    img: "assets/img/card-img/dan-burton-oNlMfgwLbWI-unsplash.jpg",
+    icon: "fa-solid fa-pizza-slice",
+    description:
+      "Duck Pizzería, competidores directos de OhMyPizza y Waldos. Abiertos de Lunes a Viernes, de 11am - 8pm",
+    phone: "222-2222",
+    email: "duckspa@gmail.com",
+  },
+  {
+    name: "Duck Café",
+    category: "Cafetería",
+    tags: "café, cafetería, restaurant",
+    img: "assets/img/card-img/dan-burton-wHsOV75Xi8Y-unsplash.jpg",
+    icon: "fa-solid fa-mug-hot",
+    description:
+      "Cafetería de renombre, con granos de café molidos el mismo día para vivir la mejor experiencia del café.",
+    phone: "333-3333",
+    email: "duckspa@gmail.com",
+  },
+  {
+    name: "DUCK Finanzas",
+    category: "Finanzas",
+    tags: "finanzas, dinero, contador",
+    img: "assets/img/card-img/nick-chong-N__BnvQ_w18-unsplash.jpg",
+    icon: "fa-solid fa-money-bill",
+    description:
+      "Servicios financieros DUCK, disponibles de lunes a viernes, de 8am - 6pm.",
+    phone: "444-4444",
+    email: "duckspa@gmail.com",
+  },
+  {
+    name: "Duck Boutique",
+    category: "Boutique",
+    tags: "boutique, ropa",
+    img: "assets/img/card-img/s-o-c-i-a-l-c-u-t-7KkDiSs5UdQ-unsplash.jpg",
+    icon: "fa-solid fa-shirt",
+    description:
+      "Ropa importada directamente desde Europa, manufacturada por las fábricas más importantes del continente.",
+    phone: "555-5555",
+    email: "duckspa@gmail.com",
+  },
+  {
+    name: "Duck Tutores",
+    category: "Educación",
+    tags: "profesores, educación",
+    img: "assets/img/card-img/scott-graham-5fNmWej4tAA-unsplash.jpg",
+    icon: "fa-solid fa-pen",
+    description:
+      "Buscamos prestar la mejor experiencia que pueda brindar la enseñanza personalizada. Se realizan clases personales y en grupo.",
+    phone: "666-6666",
+    email: "duckspa@gmail.com",
+  },
+  {
+    name: "Duck RPA",
+    category: "Start Up Tecnológica",
+    tags: "tecnología, RPA",
+    img: "assets/img/card-img/shridhar-gupta-dZxQn4VEv2M-unsplash.jpg",
+    icon: "fa-solid fa-robot",
+    description:
+      "Duck RPA busca generar un avance enorme en el mundo empresarial de Chile, entregando el poder de la automatización de procesos.",
+    phone: "777-7777",
+    email: "duckspa@gmail.com",
+  },
+  {
+    name: "DUCK Design",
+    category: "Diseño",
+    tags: "diseño, creatividad, arte",
+    img: "assets/img/card-img/studio-republic-fotKKqWNMQ4-unsplash.jpg",
+    icon: "fa-solid fa-brush",
+    description:
+      "Junto a un equipo de profesionales artísticos multidiscplinario, buscamos satisfacer todas las necesidades orientadas al diseño de nuestros clientes.",
+    phone: "888-8888",
+    email: "duckspa@gmail.com",
+  },
+  {
+    name: "Abogados Duck & Duck",
+    category: "Asesoría Legal",
+    tags: "abogado, legalidad, leyes",
+    img: "assets/img/card-img/towfiqu-barbhuiya-nApaSgkzaxg-unsplash.jpg",
+    icon: "fa-solid fa-scale-balanced",
+    description: "¿Legal problems? ¡Better call Saul! - Saul Goodman",
+    phone: "(505) 503-4455",
+    email: "duckspa@gmail.com",
+  },
+];
